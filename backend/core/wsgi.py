2e1ed2c89477ee8f9436ae98fdd8e767e2472c0b
Atif Raza <matifraza512@gmail.com>
2026-05-03 20:22:21 +0500

    fix: restore wsgi application variable

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -1,4 +1,8 @@
 import os
 from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-app = get_wsgi_application()
+
+application = get_wsgi_application()
+
+app = application
